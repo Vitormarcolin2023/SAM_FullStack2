7d9cb0e42dc630b0e8fddd33ec827e256ac03256
cecimoura <cecimoura91@gmail.com>
2025-08-14 15:22:51 -0300

    Enum AreaDeAtuacao e StatusMentor criadas

---
+++ src/main/java/com/br/SAM_FullStack/SAM_FullStack/model/AreaDeAtuacao.java
@@ -0,0 +1,11 @@
+package com.br.SAM_FullStack.SAM_FullStack.model;
+
+public enum AreaDeAtuacao {
+    AGRÁRIAS_E_BIOLOGIA,
+    EDUCAÇÃO,
+    ENGENHARIA_E_ARQUITETURA,
+    GESTÃO,
+    SAÚDE,
+    SOCIAIS,
+    TECNOLOGIA
+}
