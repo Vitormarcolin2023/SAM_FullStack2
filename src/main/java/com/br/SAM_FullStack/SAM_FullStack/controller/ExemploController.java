18c25bde1d3620bc490911c00344d3bddbb9017f
Vitor Hugo Marcolin <marcolinvh@gmail.com>
2025-08-14 13:52:05 -0300

    Subindo projeto base

---
+++ src/main/java/com/br/SAM_FullStack/SAM_FullStack/controller/ExemploController.java
@@ -0,0 +1,7 @@
+package com.br.SAM_FullStack.SAM_FullStack.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ExemploController {
+}
---
+++ src/main/java/com/br/SAM_FullStack/SAM_FullStack/service/ExemploService.java
@@ -0,0 +1,7 @@
+package com.br.SAM_FullStack.SAM_FullStack.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExemploService {
+}
