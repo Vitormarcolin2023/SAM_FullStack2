38606c758b49758151bbf0d5477d4103781ad564
Samara-Laly <samaraachterberg16@outlook.com>
2025-08-16 13:14:23 -0300

    Classes Projetos quase pronto

--- src/main/java/com/br/SAM_FullStack/SAM_FullStack/controller/ProjetoController.java
+++ src/main/java/com/br/SAM_FullStack/SAM_FullStack/controller/ProjetoController.java
@@ -1,15 +1,90 @@
 package com.br.SAM_FullStack.SAM_FullStack.controller;
 
+import com.br.SAM_FullStack.SAM_FullStack.model.AreaDeAtuacao;
+import com.br.SAM_FullStack.SAM_FullStack.model.Projeto;
 import com.br.SAM_FullStack.SAM_FullStack.service.ProjetoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/Projetos")
-public class ProjetoController {
+import java.util.List;
+
+    @RestController
+    @RequestMapping("/projetos")
+    public class ProjetoController {
 
     @Autowired
     private ProjetoService projetoService;
-    
-}
+
+    public ProjetoController(ProjetoService projetoService) {
+        this.projetoService = projetoService;
+    }
+
+    //LISTAR TODOS OS PROJETO
+    @GetMapping("/findAll")
+    public ResponseEntity<List<Projeto>> listAll() {
+        try {
+            var result = projetoService.listAll();
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+    //BUSCAR POR ID
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<Object> findById(@PathVariable Long id) {
+        try {
+            Projeto projeto = projetoService.findById(id);
+            return ResponseEntity.ok(projeto);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Projeto com ID " + id + " não encontrado.");
+        }
+    }
+    @GetMapping("/buscar-por-nome")
+    public List<Projeto> buscarPorNome(@RequestParam String nome){
+        return projetoService.buscarPorNome(nome);
+    }
+    @GetMapping("/buscar-por-atuacao")
+    public List<Projeto> buscarPorAtuacao (@RequestParam AreaDeAtuacao areaDeAtuacao){
+        return projetoService.buscarPorAreaAtuacao(areaDeAtuacao);
+    }
+
+    //SALVAR PROJETO
+    @PostMapping("/save")
+    public ResponseEntity<Object> save(@RequestBody Projeto projeto) {
+        try {
+            Projeto savedProjeto = projetoService.save(projeto);
+            return ResponseEntity.ok(savedProjeto);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Erro ao salvar projeto: " + e.getMessage());
+        }
+    }
+
+    //ATUALIZAR PROJETO
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Object> update (@PathVariable Long id, @RequestBody Projeto projeto) {
+        try {
+            Projeto projetoAtualizado = projetoService.update(id, projeto);
+            return ResponseEntity.ok(projetoAtualizado);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Não foi possível atualizar o projeto. Verifique o ID ou os dados fornecidos.");
+        }
+    }
+        //DELETAR PROJETO
+        @DeleteMapping("/delete/{id}")
+        public ResponseEntity<String> delete (@PathVariable Long id){
+            try {
+                projetoService.delete(id);
+                return ResponseEntity.ok("Projeto excluído com sucesso");
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("Não foi possível excluir o projeto. O ID fornecido não existe.");
+            }
+        }
+    }
+
+
--- src/main/java/com/br/SAM_FullStack/SAM_FullStack/repository/ProjetoRepository.java
+++ src/main/java/com/br/SAM_FullStack/SAM_FullStack/repository/ProjetoRepository.java
@@ -1,9 +1,18 @@
 package com.br.SAM_FullStack.SAM_FullStack.repository;
 
+import com.br.SAM_FullStack.SAM_FullStack.model.AreaDeAtuacao;
 import com.br.SAM_FullStack.SAM_FullStack.model.Projeto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ProjetoRepository extends JpaRepository <Projeto, Long> {
 
+    //FILTRA PROJETOS QUE CONTENHAM PARTE DO NOME
+    List<Projeto> findByNomeIgnoreCaseContaining(String nome);
 
+    //FILTRA POR AREA DE ATUAÇÃO
+    List<Projeto> findByAreaAtuacao(AreaDeAtuacao areaDeAtuacao);
 }
