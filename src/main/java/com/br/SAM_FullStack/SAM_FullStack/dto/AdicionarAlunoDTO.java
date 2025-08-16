d862e1109ee8d5ccd022e0e56a10cb566d8180c7
Josiane Steiernagel <josianesteiernagel@gmail.com>
2025-08-16 17:12:02 -0300

    Criação de uma nova DTO para adicionar alunos em um grupo já existente

---
+++ src/main/java/com/br/SAM_FullStack/SAM_FullStack/dto/AdicionarAlunoDTO.java
@@ -0,0 +1,13 @@
+package com.br.SAM_FullStack.SAM_FullStack.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdicionarAlunoDTO {
+        private Integer idAdmin;
+        private long idGrupo;
+        private Integer idAluno;
+}
+
