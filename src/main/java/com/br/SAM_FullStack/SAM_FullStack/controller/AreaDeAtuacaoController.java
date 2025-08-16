6e4b5c15549ac31e47cc8c440c5a1ab9d65a97b2
unknown <marcolinvh@gmail.com>
2025-08-16 13:38:24 -0300

    Subindo os controller para fazer a atualização e a deleção

--- src/main/java/com/br/SAM_FullStack/SAM_FullStack/controller/AreaDeAtuacaoController.java
+++ src/main/java/com/br/SAM_FullStack/SAM_FullStack/controller/AreaDeAtuacaoController.java
@@ -1,7 +1,9 @@
 package com.br.SAM_FullStack.SAM_FullStack.controller;
 
 import com.br.SAM_FullStack.SAM_FullStack.model.AreaDeAtuacao;
+import com.br.SAM_FullStack.SAM_FullStack.model.Curso;
 import com.br.SAM_FullStack.SAM_FullStack.service.AreaDeAtuacaoService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -36,6 +38,26 @@ public class AreaDeAtuacaoController {
             // Logar o erro
             ex.printStackTrace();
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR); // Status 500
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update (@PathVariable Long id, @Valid @RequestBody AreaDeAtuacao areaDeAtuacaoUpdate){
+        try{
+            AreaDeAtuacao areaDeAtuacaoAtualizado = areaDeAtuacaoService.update(id, areaDeAtuacaoUpdate);
+            return ResponseEntity.ok(areaDeAtuacaoAtualizado);
+        }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/delet/{id}")
+    public ResponseEntity<?> delete (@PathVariable Long id){
+        try{
+            areaDeAtuacaoService.delete(id);
+            return ResponseEntity.noContent().build();
+        }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
     }
 
