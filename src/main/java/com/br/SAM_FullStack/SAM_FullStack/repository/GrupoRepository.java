7fc40bb95a0bfb29f48db18b6aa335852d2045db
Josiane Steiernagel <josianesteiernagel@gmail.com>
2025-08-15 23:05:22 -0300

    criação de um método de filtragem

--- src/main/java/com/br/SAM_FullStack/SAM_FullStack/repository/GrupoRepository.java
+++ src/main/java/com/br/SAM_FullStack/SAM_FullStack/repository/GrupoRepository.java
@@ -3,5 +3,10 @@ package com.br.SAM_FullStack.SAM_FullStack.repository;
 import com.br.SAM_FullStack.SAM_FullStack.model.Grupo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GrupoRepository extends JpaRepository<Grupo, Long> {
+
+    // Retorna o grupo em que o aluno está inserido
+    Optional<Grupo> findByAlunosId(Integer id);
 }
