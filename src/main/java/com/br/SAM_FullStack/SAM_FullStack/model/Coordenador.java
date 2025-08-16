fd245259097af3b6d35435ca5516072e7ca16e18
unknown <marcolinvh@gmail.com>
2025-08-16 19:37:55 -0300

    Adicionado @Table(name = tb_coordenacao) no model coordenação

--- src/main/java/com/br/SAM_FullStack/SAM_FullStack/model/Coordenador.java
+++ src/main/java/com/br/SAM_FullStack/SAM_FullStack/model/Coordenador.java
@@ -1,9 +1,6 @@
 package com.br.SAM_FullStack.SAM_FullStack.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +11,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "tb_coordenacao")
 public class Coordenador{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
