fdedeacc0037b6a54995053423a981f651dff1ca
Samara-Laly <samaraachterberg16@outlook.com>
2025-08-15 22:32:26 -0300

    Adicionando classes Projeto

--- src/main/java/com/br/SAM_FullStack/SAM_FullStack/controller/ProjetoController.java
+++ src/main/java/com/br/SAM_FullStack/SAM_FullStack/controller/ProjetoController.java
@@ -1,4 +1,15 @@
 package com.br.SAM_FullStack.SAM_FullStack.controller;
 
+import com.br.SAM_FullStack.SAM_FullStack.service.ProjetoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Projetos")
 public class ProjetoController {
+
+    @Autowired
+    private ProjetoService projetoService;
+    
 }
--- src/main/java/com/br/SAM_FullStack/SAM_FullStack/repository/ProjetoRepository.java
+++ src/main/java/com/br/SAM_FullStack/SAM_FullStack/repository/ProjetoRepository.java
@@ -1,4 +1,9 @@
 package com.br.SAM_FullStack.SAM_FullStack.repository;
 
-public class ProjetoRepository {
+import com.br.SAM_FullStack.SAM_FullStack.model.Projeto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjetoRepository extends JpaRepository <Projeto, Long> {
+
+
 }
