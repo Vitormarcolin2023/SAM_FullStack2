76de13f74f8efdbb533a5da4850f46f149658b5a
Josiane Steiernagel <josianesteiernagel@gmail.com>
2025-08-16 17:16:55 -0300

    criacao de metodo para encontrar grupos em que o aluno esteja com o status do Enum em 'AGUARDANDO' para fazer a filtragem de grupos que solicitaram a exclusao de algum integrante

--- src/main/java/com/br/SAM_FullStack/SAM_FullStack/repository/GrupoRepository.java
+++ src/main/java/com/br/SAM_FullStack/SAM_FullStack/repository/GrupoRepository.java
@@ -1,12 +1,18 @@
 package com.br.SAM_FullStack.SAM_FullStack.repository;
 
 import com.br.SAM_FullStack.SAM_FullStack.model.Grupo;
+import com.br.SAM_FullStack.SAM_FullStack.model.StatusAlunoGrupo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GrupoRepository extends JpaRepository<Grupo, Long> {
 
     // Retorna o grupo em que o aluno está inserido
     Optional<Grupo> findByAlunosId(Integer id);
+
+    List<Grupo> findByAlunosStatusAlunoGrupo(StatusAlunoGrupo statusAlunoGrupo);
+
+
 }
