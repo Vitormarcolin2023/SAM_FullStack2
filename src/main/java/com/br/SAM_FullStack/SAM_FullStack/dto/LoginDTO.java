0b6dbd91f298f2be804f39045f248799cddd6fd0
Josiane Steiernagel <josianesteiernagel@gmail.com>
2025-09-13 18:35:42 -0300

    criação de um DTO para recber as informações de login do usuário

--- src/main/java/com/br/SAM_FullStack/SAM_FullStack/dto/LoginDTO.java
+++ src/main/java/com/br/SAM_FullStack/SAM_FullStack/dto/LoginDTO.java
@@ -13,4 +13,5 @@ public class LoginDTO {
 
     private String email;
     private String senha;
+    private String role;
 }
--- src/main/java/com/br/SAM_FullStack/SAM_FullStack/login/Usuario.java
+++
@@ -1,6 +0,0 @@
-package com.br.SAM_FullStack.SAM_FullStack.login;
-
-public class Usuario {
-
-
-}
