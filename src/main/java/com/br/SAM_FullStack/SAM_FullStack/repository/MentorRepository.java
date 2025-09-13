a55c9b161ef102e94a1a57e817b936cb97a9f942
Josiane Steiernagel <josianesteiernagel@gmail.com>
2025-09-13 18:37:55 -0300

    adição de métodos de busca nos Repositories para validar o login do usuário

--- src/main/java/com/br/SAM_FullStack/SAM_FullStack/repository/MentorRepository.java
+++ src/main/java/com/br/SAM_FullStack/SAM_FullStack/repository/MentorRepository.java
@@ -1,8 +1,12 @@
 package com.br.SAM_FullStack.SAM_FullStack.repository;
 
+import com.br.SAM_FullStack.SAM_FullStack.model.Aluno;
 import com.br.SAM_FullStack.SAM_FullStack.model.Mentor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MentorRepository extends JpaRepository<Mentor, Long> {
 
+    Optional<Mentor> findByEmailAndSenha(String email, String senha);
 }
