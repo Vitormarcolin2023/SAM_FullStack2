e006ded89a2abae6fbf68e0add397f9713dc8d6e
Josiane Steiernagel <josianesteiernagel@gmail.com>
2025-09-13 18:41:15 -0300

    criação de uma classe filtro para validar o Token de acesso do usuário

--- src/main/java/com/br/SAM_FullStack/SAM_FullStack/autenticacao/JwtFilter.java
+++ src/main/java/com/br/SAM_FullStack/SAM_FullStack/autenticacao/JwtFilter.java
@@ -1,4 +1,33 @@
 package com.br.SAM_FullStack.SAM_FullStack.autenticacao;
 
-public class JWTFilter {
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+
+public class JwtFilter implements Filter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        String authHeader = req.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            try {
+                String email = tokenService.validateToken(token);
+                req.setAttribute("usuarioLogado", email);
+            } catch (Exception e) {
+                throw new ServletException("Token inválido ou expirado");
+            }
+        }
+        chain.doFilter(request, response);
+    }
 }
+
