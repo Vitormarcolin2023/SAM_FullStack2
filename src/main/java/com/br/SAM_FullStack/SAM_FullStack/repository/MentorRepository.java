38aff2095f5ece460cf39a4dc4e3aa28c361030a
Vitormarcolin2023 <marcolinvh@gmail.com>
2025-09-29 21:20:05 -0300

    Adiciona filtro por status na busca de mentores por área - ATIVO

--- src/main/java/com/br/SAM_FullStack/SAM_FullStack/repository/MentorRepository.java
+++ src/main/java/com/br/SAM_FullStack/SAM_FullStack/repository/MentorRepository.java
@@ -1,6 +1,6 @@
 package com.br.SAM_FullStack.SAM_FullStack.repository;
 
-import com.br.SAM_FullStack.SAM_FullStack.model.Aluno;
+import com.br.SAM_FullStack.SAM_FullStack.model.StatusMentor;
 import com.br.SAM_FullStack.SAM_FullStack.model.Mentor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -12,5 +12,5 @@ public interface MentorRepository extends JpaRepository<Mentor, Long> {
     Optional<Mentor> findByEmailAndSenha(String email, String senha);
     Optional<Mentor> findByEmail(String email);
 
-    List<Mentor> findByAreaDeAtuacaoId(Long idArea);
+    List<Mentor> findByAreaDeAtuacaoIdAndStatusMentor(Long idArea, StatusMentor status);
 }
