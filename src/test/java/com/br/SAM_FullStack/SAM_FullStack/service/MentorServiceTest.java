37b75fc50c3da570b297633c450b0f4a29c97d82
cecimoura <cecimoura91@gmail.com>
2025-10-19 18:43:49 -0300

    teste de integração Mock Repository

--- src/test/java/com/br/SAM_FullStack/SAM_FullStack/service/MentorServiceTest.java
+++ src/test/java/com/br/SAM_FullStack/SAM_FullStack/service/MentorServiceTest.java
@@ -42,6 +42,85 @@ public class MentorServiceTest {
 
     @BeforeEach
     void setUp() {
+        area = new AreaDeAtuacao(1L, "Arquitetura");
 
+        mentor = new Mentor(
+                1L, "João Teste", "12345678900", "joao@teste.com",
+                "senha123", "999999999", "5 anos", StatusMentor.ATIVO,
+                TipoDeVinculo.CLT, area, null, null, "Resumo Teste", null
+        );
+    }
+
+    //testes de integração -Mock Repository
+    @Test
+    @DisplayName("TESTE DE INTEGRAÇÃO – listagem de todos os Mentores (Lista preenchida)")
+    void listAll_DeveRetornarListaDeMentores() {
+        Mentor mentor2 = new Mentor();
+        List<Mentor> listaEsperada = Arrays.asList(mentor, mentor2);
+        when(mentorRepository.findAll()).thenReturn(listaEsperada);
+
+        //listagem
+        List<Mentor> listaAtual = mentorService.listAll();
+
+        // Verificação
+        assertFalse(listaAtual.isEmpty(), "A lista não deveria estar vazia");
+        assertEquals(2, listaAtual.size(), "A lista deve conter 2 mentores");
+        verify(mentorRepository, times(1)).findAll();
+    }
+
+    @Test
+    @DisplayName("TESTE DE INTEGRAÇÃO – busca de Mentor por ID")
+    void findById_DeveRetornarMentorQuandoEncontrado() {
+        //Mocka o Repositório para retornar o Mentor
+        when(mentorRepository.findById(mentor.getId())).thenReturn(Optional.of(mentor));
+
+        Mentor mentorEncontrado = mentorService.findById(mentor.getId());
+
+        // Verificação
+        assertNotNull(mentorEncontrado);
+        assertEquals(mentor.getNome(), mentorEncontrado.getNome());
+        verify(mentorRepository, times(1)).findById(mentor.getId());
+    }
+
+    @Test
+    @DisplayName("TESTE DE INTEGRAÇÃO – lançar runtimeException  ao buscar Mentor por ID inexistente")
+    void findById_MentorNaoEncontrado() {
+        // Mocka o Repositório para retornar Optional vazio
+        when(mentorRepository.findById(99L)).thenReturn(Optional.empty());
+
+        // ação e verificação
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            mentorService.findById(99L);
+        });
+
+        assertEquals("Mentor não encontrado", exception.getMessage());
+        verify(mentorRepository, times(1)).findById(99L);
+    }
+
+    @Test
+    @DisplayName("TESTE DE INTEGRAÇÃO – Delete Mentor")
+    void delete_DeveDeletarMentorQuandoEncontrado() {
+        when(mentorRepository.findById(mentor.getId())).thenReturn(Optional.of(mentor));
+
+        mentorService.delete(mentor.getId());
+
+        // Verificação: Deve chamar findById e em seguida o delete do repository
+        verify(mentorRepository, times(1)).findById(mentor.getId());
+        verify(mentorRepository, times(1)).delete(mentor);
+    }
+
+    @Test
+    @DisplayName("TESTE DE INTEGRAÇÃO – Lança exceção ao tentar deletar Mentor inexistente")
+    void delete_ExcecaoQuandoMentorNaoEncontrado() {
+        //Mockando o findById para Optional vazio
+         when(mentorRepository.findById(99L)).thenReturn(Optional.empty());
+
+        // Ação e Verificação
+        assertThrows(RuntimeException.class, () -> {
+            mentorService.delete(99L);
+        });
+
+        // O metodo delete do repository NÃO deve ser chamado
+        verify(mentorRepository, never()).delete(any(Mentor.class));
     }
 }
