4dedfa080a565740ebf11ecf1069515fcb4fbf9a
Josiane Steiernagel <josianesteiernagel@gmail.com>
2025-10-19 21:03:36 -0300

    testa todas as exceções de salvar grupo e testa cenário de sucesso

--- src/test/java/com/br/SAM_FullStack/SAM_FullStack/service/GrupoServiceTest.java
+++ src/test/java/com/br/SAM_FullStack/SAM_FullStack/service/GrupoServiceTest.java
@@ -1,6 +1,8 @@
 package com.br.SAM_FullStack.SAM_FullStack.service;
 
+import com.br.SAM_FullStack.SAM_FullStack.dto.GrupoDTO;
 import com.br.SAM_FullStack.SAM_FullStack.model.*;
+import com.br.SAM_FullStack.SAM_FullStack.repository.AlunoRepository;
 import com.br.SAM_FullStack.SAM_FullStack.repository.GrupoRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -9,147 +11,182 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class GrupoServiceTest {
 
     @Autowired
-    GrupoService grupoService;
+    private GrupoService grupoService;
 
     @MockitoBean
-    GrupoRepository grupoRepository;
+    private GrupoRepository grupoRepository;
+    @MockitoBean
+    private AlunoRepository alunoRepository;
+
+    private Aluno aluno1;
+    private Aluno aluno5;
+    private Grupo grupo1;
+    private Grupo grupo2;
+    private Grupo grupo3;
+    private GrupoDTO grupoSalvarSucesso;
 
     @BeforeEach
-    void setup(){
-        AreaDeAtuacao areaDeAtuacao1 = new AreaDeAtuacao(1L, "Tecnologia");
-        AreaDeAtuacao areaDeAtuacao2 = new AreaDeAtuacao(2L, "Saúde");
+    void setup() {
+        AreaDeAtuacao area1 = new AreaDeAtuacao(1L, "Tecnologia");
+        AreaDeAtuacao area2 = new AreaDeAtuacao(2L, "Saúde");
 
-        Curso curso1 = new Curso(1L, "Engenharia de Software", areaDeAtuacao1);
-        Curso curso2 = new Curso(2L, "Veterinária", areaDeAtuacao2);
+        Curso curso1 = new Curso(1L, "Engenharia de Software", area1);
+        Curso curso2 = new Curso(2L, "Veterinária", area2);
 
-        Aluno aluno1 = new Aluno();
-        aluno1.setId(1L);
-        aluno1.setNome("Ana Silva");
-        aluno1.setRa(1001);
-        aluno1.setSenha("senha123");
-        aluno1.setEmail("ana.silva@email.com");
-        aluno1.setCurso(curso1);
-        aluno1.setStatusAlunoGrupo(StatusAlunoGrupo.ATIVO);
+        aluno1 = new Aluno(1L, "Ana Silva", 1001, "senha123", "ana.silva@email.com", curso1, StatusAlunoGrupo.ATIVO);
+        Aluno aluno2 = new Aluno(2L, "Bruno Costa", 1002, "senha123", "bruno.costa@email.com", curso1, StatusAlunoGrupo.ATIVO);
+        Aluno aluno3 = new Aluno(3L, "Carla Mendes", 1003, "senha123", "carla.mendes@email.com", curso1, StatusAlunoGrupo.ATIVO);
+        Aluno aluno4 = new Aluno(4L, "Diego Oliveira", 1004, "senha123", "diego.oliveira@email.com", curso1, StatusAlunoGrupo.ATIVO);
 
-        Aluno aluno2 = new Aluno();
-        aluno2.setId(2L);
-        aluno2.setNome("Bruno Costa");
-        aluno2.setRa(1002);
-        aluno2.setSenha("senha123");
-        aluno2.setEmail("bruno.costa@email.com");
-        aluno2.setCurso(curso1);
-        aluno2.setStatusAlunoGrupo(StatusAlunoGrupo.ATIVO);
+        aluno5 = new Aluno(5L, "Elisa Fernandes", 1005, "senha123", "elisa.fernandes@email.com", curso2, StatusAlunoGrupo.ATIVO);
+        Aluno aluno6 = new Aluno(6L, "Fábio Santos", 1006, "senha123", "fabio.santos@email.com", curso2, StatusAlunoGrupo.ATIVO);
+        Aluno aluno7 = new Aluno(7L, "Gabriela Lima", 1007, "senha123", "gabriela.lima@email.com", curso2, StatusAlunoGrupo.AGUARDANDO);
+        Aluno aluno8 = new Aluno(8L, "Regina Lima", 1008, "senha123", "regina.lima@email.com", curso2, StatusAlunoGrupo.AGUARDANDO);
 
-        Aluno aluno3 = new Aluno();
-        aluno3.setId(3L);
-        aluno3.setNome("Carla Mendes");
-        aluno3.setRa(1003);
-        aluno3.setSenha("senha123");
-        aluno3.setEmail("carla.mendes@email.com");
-        aluno3.setCurso(curso1);
-        aluno3.setStatusAlunoGrupo(StatusAlunoGrupo.ATIVO);
+        grupo1 = new Grupo(1L, "Grupo Eng. Soft.", StatusGrupo.ATIVO, aluno1, List.of(aluno1, aluno2, aluno3, aluno4));
+        grupo3 = new Grupo(3L, "Grupo Eng. Soft Arquivado", StatusGrupo.ARQUIVADO, aluno1, List.of(aluno1, aluno2, aluno3, aluno4));
+        grupo2 = new Grupo(2L, "Grupo Veterinária", StatusGrupo.ARQUIVADO, aluno5, List.of(aluno5, aluno6, aluno7));
 
-        Aluno aluno4 = new Aluno();
-        aluno4.setId(4L);
-        aluno4.setNome("Diego Oliveira");
-        aluno4.setRa(1004);
-        aluno4.setSenha("senha123");
-        aluno4.setEmail("diego.oliveira@email.com");
-        aluno4.setCurso(curso1);
-        aluno4.setStatusAlunoGrupo(StatusAlunoGrupo.ATIVO);
+        aluno1.setGrupos(List.of(grupo1, grupo3));
+        aluno2.setGrupos(List.of(grupo1, grupo3));
+        aluno3.setGrupos(List.of(grupo1, grupo3));
+        aluno4.setGrupos(List.of(grupo1, grupo3));
 
-        Aluno aluno5 = new Aluno();
-        aluno5.setId(5L);
-        aluno5.setNome("Elisa Fernandes");
-        aluno5.setRa(1005);
-        aluno5.setSenha("senha123");
-        aluno5.setEmail("elisa.fernandes@email.com");
-        aluno5.setCurso(curso2);
-        aluno5.setStatusAlunoGrupo(StatusAlunoGrupo.ATIVO);
+        aluno5.setGrupos(new ArrayList<>(List.of(grupo2)));
+        aluno6.setGrupos(new ArrayList<>(List.of(grupo2)));
+        aluno7.setGrupos(new ArrayList<>(List.of(grupo2)));
+        aluno8.setGrupos(List.of(grupo1));
 
-        Aluno aluno6 = new Aluno();
-        aluno6.setId(6L);
-        aluno6.setNome("Fábio Santos");
-        aluno6.setRa(1006);
-        aluno6.setSenha("senha123");
-        aluno6.setEmail("fabio.santos@email.com");
-        aluno6.setCurso(curso2);
-        aluno6.setStatusAlunoGrupo(StatusAlunoGrupo.ATIVO);
+        grupoSalvarSucesso = new GrupoDTO(10L, "Grupo salvo com sucesso", 5L, List.of(5L, 6L, 7L));
 
-        Aluno aluno7 = new Aluno();
-        aluno7.setId(7L);
-        aluno7.setNome("Gabriela Lima");
-        aluno7.setRa(1007);
-        aluno7.setSenha("senha123");
-        aluno7.setEmail("gabriela.lima@email.com");
-        aluno7.setCurso(curso2);
-        aluno7.setStatusAlunoGrupo(StatusAlunoGrupo.ATIVO);
-
-        List<Aluno> alunosGrupo1 = List.of(aluno1, aluno2, aluno3, aluno4);
-        List<Aluno> alunosGrupo2 = List.of(aluno5, aluno6, aluno7);
-
-        Grupo grupo1 = new Grupo(1L, "Grupo Eng. Soft.", StatusGrupo.ATIVO, aluno1, alunosGrupo1);
-        Grupo grupo2 = new Grupo(2L, "Grupo Veterinária", StatusGrupo.ATIVO, aluno5, alunosGrupo2);
-
-        List<Grupo> grupos = List.of(grupo2, grupo1);
+        List<Grupo> todosGrupos = List.of(grupo1, grupo2, grupo3);
         List<Grupo> gruposAluno1 = List.of(grupo1);
 
-        when(this.grupoRepository.findAll()).thenReturn(grupos);
-        when(this.grupoRepository.findById(2L)).thenReturn(Optional.of(grupo2));
-        when(this.grupoRepository.findByStatusGrupoAndAlunosId(StatusGrupo.ATIVO, aluno1.getId())).thenReturn(gruposAluno1);
-    }
-
-    @Test
-    @DisplayName("Deve retornar uma lista com todos os grupos salvos")
-    void buscar_deveRetornarTodosOsGrupos (){
-        List<Grupo> response = grupoService.findAll();
-        assertEquals(2, response.size());
-    }
-
-    @Test
-    @DisplayName("Deve retornar um objeto grupo com o id que foi passado como parâmetro")
-    void buscarPorId_quandoExiste_deveRetornarGrupo(){
-        Grupo response = grupoService.findById(2L);
-        assertEquals(2L, response.getId());
-        assertEquals(3, response.getAlunos().size());
-    }
-
-    @Test
-    @DisplayName("Deve retornar uma exceção quando o id passado não existir")
-    void buscarPorId_quandoNaoExiste_deveCairEmException(){
-        assertThrows(Exception.class, () -> {
-            Grupo response = grupoService.findById(-4L);
+        when(grupoRepository.findAll()).thenReturn(todosGrupos);
+        when(grupoRepository.findById(1L)).thenReturn(Optional.of(grupo1));
+        when(grupoRepository.findById(2L)).thenReturn(Optional.of(grupo2));
+        when(grupoRepository.findById(-1L)).thenReturn(Optional.empty());
+        when(grupoRepository.findByStatusGrupoAndAlunosId(StatusGrupo.ATIVO, aluno1.getId())).thenReturn(gruposAluno1);
+        when(alunoRepository.findById(-41L)).thenReturn(Optional.empty());
+        when(alunoRepository.findById(1L)).thenReturn(Optional.of(aluno1));
+        when(alunoRepository.findById(5L)).thenReturn(Optional.of(aluno5));
+        when(alunoRepository.findById(6L)).thenReturn(Optional.of(aluno6));
+        when(alunoRepository.findAllById(List.of(1L, 2L, 3L, 4L))).thenReturn(List.of(aluno1, aluno2, aluno3, aluno4));
+        when(alunoRepository.findAllById(List.of(5L, 6L))).thenReturn(List.of(aluno5, aluno6));
+        when(alunoRepository.findAllById(List.of(6L, 7L, 8L))).thenReturn(List.of(aluno6, aluno7, aluno8));
+        when(alunoRepository.findAllById(List.of(5L, 6L, 7L))).thenReturn(List.of(aluno5, aluno6, aluno7));
+        when(grupoRepository.save(any(Grupo.class))).thenAnswer(invocation -> {
+            Grupo grupo = invocation.getArgument(0);
+            grupo.setId(10L);
+            return grupo;
         });
     }
 
     @Test
-    @DisplayName("Deve retornar um grupo que o aluno participa, com base no aluno passado como parâmetro - um grupo porque o aluno pode ter apenas um grupo ativo")
-    void buscarPorAlunoId_quandoExiste_deveRetornarGrupoAtivoDoAluno(){
-        AreaDeAtuacao areaDeAtuacao1 = new AreaDeAtuacao(1L, "Tecnologia");
-        Curso curso1 = new Curso(1L, "Engenharia de Software", areaDeAtuacao1);
+    @DisplayName("Deve retornar todos os grupos")
+    void buscar_deveRetornarTodosOsGrupos() {
+        List<Grupo> response = grupoService.findAll();
+        assertEquals(3, response.size());
+    }
 
-        Aluno aluno1 = new Aluno();
-        aluno1.setId(1L);
-        aluno1.setNome("Ana Silva");
-        aluno1.setRa(1001);
-        aluno1.setSenha("senha123");
-        aluno1.setEmail("ana.silva@email.com");
-        aluno1.setCurso(curso1);
-        aluno1.setStatusAlunoGrupo(StatusAlunoGrupo.ATIVO);
+    @Test
+    @DisplayName("Deve retornar o grupo pelo ID")
+    void buscarPorId_quandoExiste_deveRetornarGrupo() {
+        Grupo response = grupoService.findById(1L);
+        assertEquals(1L, response.getId());
+        assertEquals(4, response.getAlunos().size());
+    }
 
+    @Test
+    @DisplayName("Deve lançar exceção quando o ID não existir")
+    void buscarPorId_quandoNaoExiste_deveCairEmException() {
+        assertThrows(RuntimeException.class, () -> grupoService.findById(-1L));
+    }
+
+    @Test
+    @DisplayName("Deve retornar grupo ativo de um aluno")
+    void buscarPorAlunoId_quandoExiste_deveRetornarGrupoAtivoDoAluno() {
         Grupo response = grupoService.findByAluno(aluno1);
-        assertEquals(true, response.getAlunos().contains(aluno1));
         assertEquals(StatusGrupo.ATIVO, response.getStatusGrupo());
+        assertEquals(true, response.getAlunos().contains(aluno1));
+    }
+
+
+    // --- cenários de erros para salvar grupo
+    @Test
+    @DisplayName("Deve retornar erro ao buscar id de aluno que não existe ao tentar salvar grupo")
+    void salvarGrupo_quandoIdDeAlunoNaoExiste_deveRetornarException(){
+        assertThrows(Exception.class, () -> {
+            GrupoDTO grupoSave = new GrupoDTO(4L, "Grupo Teste Save", -41L, List.of(5L, 6L, 7L));
+            grupoService.save(grupoSave);
+        });
+    }
+
+    @Test
+    @DisplayName("Deve retornor erro ao tentar salvar um novo grupo quando o admin informado já participa de outro grupo ativo")
+    void salvarGrupo_quandoAdminJaPossuiGrupoAtivo_deveRetornarException(){
+        assertThrows(IllegalStateException.class, () -> {
+            GrupoDTO grupoErroStatus = new GrupoDTO(5L, "Grupo erro status", 1L, List.of(1L, 2L, 3L, 4L));
+            grupoService.save(grupoErroStatus);
+        });
+    }
+
+    @Test
+    @DisplayName("Deve retornar erro ao tentar salvar novo grupo quando algum aluno informado não existe")
+    void salvarGrupo_quandoAlunoInformadoNaoExiste_deveRetornarExceprion(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            GrupoDTO grupoErroAlunos = new GrupoDTO(6L, "Grupo teste erro Alunos", 5L, List.of(5L, 6L, -7L));
+            grupoService.save(grupoErroAlunos);
+        });
+    }
+
+    @Test
+    @DisplayName("Deve retornar erro ao tentar salvar grupo com qtd de alunos menor do que a permitida")
+    void salvarGrupo_quandoQtdDeAlunoEhMenorDoQuePermitida_deveRetornarErro(){
+        assertThrows(IllegalStateException.class, () -> {
+           GrupoDTO grupoErroQtdAlunos = new GrupoDTO(7L, "Grupo teste erro qtd alunos", 5L, List.of(5L, 6L));
+           grupoService.save(grupoErroQtdAlunos);
+        });
+    }
+
+    @Test
+    @DisplayName("Deve retornar erro ao tentar salvar grupo quando o id do admin não é passado cna lista de alunos")
+    void salvarGrupo_quandoIdDoAdminNaoForInformadoNaLista_deveRetornarErro(){
+        assertThrows(IllegalStateException.class, () -> {
+           GrupoDTO grupoErroListaALunos = new GrupoDTO(8L, "Grupo teste erro lista alunos", 5L, List.of(6L, 7L, 8L));
+           grupoService.save(grupoErroListaALunos);
+        });
+    }
+
+    @Test
+    @DisplayName("Deve retornar erro ao tentar salvar grupo quando algum aluno informado já participa de outro grupo ativo")
+    void salvarGrupo_quandoAlgumAlunoTemGrupoAtivo_deveRetorarErro(){
+        assertThrows(IllegalStateException.class, () ->{
+           GrupoDTO grupoErroAlunoComGrupoAtivo = new GrupoDTO(9L, "Grupo teste erro aluno com grupo ativo",
+                   6L, List.of(6L, 7L, 8L));
+           grupoService.save(grupoErroAlunoComGrupoAtivo);
+        });
+    }
+
+    // --- cenários de sucesso para salvar grupo
+
+    @Test
+    @DisplayName("Deve salvar novo grupo quando não há erros")
+    void salvarGrupo_quandoInformacoesCorretas_deveSalvar(){
+        GrupoDTO response = grupoService.save(grupoSalvarSucesso);
+        assertEquals(10L, response.id());
     }
 }
