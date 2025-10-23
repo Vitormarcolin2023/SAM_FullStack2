167eb60f46bd17e4ac9d7ea53e808ee52743c5b6
Josiane Steiernagel <josianesteiernagel@gmail.com>
2025-10-23 14:52:19 -0300

    testes de GrupoController completos

--- src/main/java/com/br/SAM_FullStack/SAM_FullStack/dto/AdicionarAlunoDTO.java
+++ src/main/java/com/br/SAM_FullStack/SAM_FullStack/dto/AdicionarAlunoDTO.java
@@ -1,10 +1,14 @@
 package com.br.SAM_FullStack.SAM_FullStack.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AdicionarAlunoDTO {
         private Long idAdmin;
         private long idGrupo;
