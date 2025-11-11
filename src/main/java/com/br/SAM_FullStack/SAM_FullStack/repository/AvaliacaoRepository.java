3d4e87538a6b5dc9ede4b12a09bccd59a39bda2b
Vitormarcolin2023 <marcolinvh@gmail.com>
2025-11-10 21:13:40 -0300

    Criado toda a estrutura para que possa receber os dados e calcular a media, para que a função mentor funcione

--- src/main/java/com/br/SAM_FullStack/SAM_FullStack/repository/AvaliacaoRepository.java
+++ src/main/java/com/br/SAM_FullStack/SAM_FullStack/repository/AvaliacaoRepository.java
@@ -1,4 +1,10 @@
 package com.br.SAM_FullStack.SAM_FullStack.repository;
 
-public class AvaliacaoRepository {
+import com.br.SAM_FullStack.SAM_FullStack.model.Avaliacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long> {
+
 }
