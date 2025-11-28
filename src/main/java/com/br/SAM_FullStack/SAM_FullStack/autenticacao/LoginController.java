b56ca07a7da95e26d88ea927252d94b1de8e652a
Josiane Steiernagel <josianesteiernagel@gmail.com>
2025-11-28 18:38:23 -0300

    corrige os testes de LoginController

--- src/main/java/com/br/SAM_FullStack/SAM_FullStack/autenticacao/LoginController.java
+++ src/main/java/com/br/SAM_FullStack/SAM_FullStack/autenticacao/LoginController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@AllArgsConstructor
 @RequestMapping("/auth")
 @CrossOrigin("*")
 public class LoginController {
